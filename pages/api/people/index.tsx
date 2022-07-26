b3afbffb3830f26991f9e56aac079985935fa9fe
Alejo <alejoivanrojas@gmail.com>
2022-07-25 21:52:23 -0300

    cors library added

--- pages/api/people/index.tsx
+++ pages/api/people/index.tsx
@@ -1,5 +1,32 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import Cors from 'cors'
+
+// Initializing the cors middleware
+// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+const cors = Cors({
+  methods: ['POST', 'GET'],
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+
 
  export const people: People[] = [ 
     {
@@ -26,9 +53,9 @@ export type People = {
 export default async function handler(req: NextApiRequest, res: NextApiResponse<People[]|any>) {
     // const resolve = await fetch("https://62b3a9264f851f87f45dfb80.mockapi.io/api/example/data")
     // const data: Data[]= await resolve.json()
+
     if(req.method === "POST"){
-      res.setHeader("Allow", "POST");
-      res.setHeader("Access-Control-Allow-Origin", "*");
+      await runMiddleware(req, res, cors)
       res.status(200).send(req.body)
     }
     else res.status(200).json(people)
